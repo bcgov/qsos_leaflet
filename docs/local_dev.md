479474d4336e9a4f91fa2650655fc55e4241d8c2
Guy Lafleur <kevin.netherton@gov.bc.ca>
2024-05-14 08:47:25 -0700

    feat: adapting the default app (#3)

---
+++ docs/local_dev.md
@@ -0,0 +1,28 @@
+# Getting Started
+
+Assuming you have created a new repo based on this template, this guide focuses on how to get started developing your leaflet app.
+
+# Pre-requisitis
+
+* [nvm](https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating)
+
+Use nvm to install npm 
+
+`nvm install 20`
+
+... or whatever version you want to run.
+
+Load Dependencies
+
+`npm install`
+
+# Running the starter app
+
+```
+cd frontend
+npm run dev
+```
+
+Should set things up allowing for local development
+
+
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,5 +1,14 @@
-import L from 'leaflet';
+import * as L from 'leaflet';
+import * as esri from 'esri-leaflet'
+// import * as esri_vect from 'esri-leaflet-vector';
+import { vectorBasemapLayer } from "esri-leaflet-vector";
+
+
 import './style.scss';
+
+// import * as esri from 'esri-leaflet';
+// import basemapLayer from 'esri-leaflet';
+// import featureLayer from 'esri-leaflet'
 
 import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
 import marker from 'leaflet/dist/images/marker-icon.png';
@@ -12,15 +21,57 @@ import '@bcgov/bootstrap-v5-theme/images/bcid-logo-rev-en.svg';
 
 
 
-delete (<any>L.Icon.Default.prototype)._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: marker2x,
-	iconUrl: marker,
-	shadowUrl: markerShadow,
-});
+window.onload = () => esriLeafletMap();
 
-window.onload = () => {
+function esriLeafletMap() {
+    const apiKey = "AAPKfeb7069c84484b8b95e784e272e814e79joMyrpeYbd_UY7-zcFyWeOGjr9x2aOkQLjQruXHPLr7ujIOGRZ8LTEg67F9cCyp";
+    const basemapEnum = "NationalGeographic";
+    const lat=54.66;
+    const lon=-130.34;
+    const zoom_lvl=5;
+
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: marker2x,
+        iconUrl: marker,
+        shadowUrl: markerShadow,
+    });
+
+    const map: L.Map = L.map("map", {
+        minZoom: 2
+      })
+
+      map.setView([lat, lon], zoom_lvl);
+
+      // seems to be the correct way to do this, but keeps raising errors
+      // ----------------------------------------------------------------
+      // vectorBasemapLayer("ArcGIS:Streets", {
+      //   token: apiKey, version: '1.0.0'
+      // }).addTo(map);
+
+      // workaround till figure out above
+      esri.basemapLayer(basemapEnum, { token: apiKey }).addTo(map);
+
+      const clever_map_data_url = 'https://services6.arcgis.com/ubm4tcTYICKBpist/arcgis/rest/services/CLM_MapHub_forecast/FeatureServer/0'
+      var clever_output: any = esri.featureLayer({
+        url: clever_map_data_url
+      });
+      clever_output.addTo(map);
+
+
+}
+    
+function superSimpleLeafletMap()    
+{
+
+    delete (<any>L.Icon.Default.prototype)._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: marker2x,
+        iconUrl: marker,
+        shadowUrl: markerShadow,
+    });
+    
 	let map: L.Map = L.map('map').setView([51.505, -0.09], 13);
 
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
